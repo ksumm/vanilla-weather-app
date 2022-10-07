f939c7a05ce1d38f3ae41aaab2b823bbdecd5dd4
Oksana Mashyntseva <67964372+ksumm@users.noreply.github.com>
2022-10-07 10:41:37 +0100

    showing the current city, temperature, humidity, wind

---
+++ src/app.js
@@ -0,0 +1,19 @@
+function displayTemperature(response){
+  console.log(response.data);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+
+}
+
+let apiKey = "c197bfddbccf715635ed21140b4bacbb";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Limerick&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
