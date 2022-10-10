ccdaf6c2b2dd0d8d317bfd215920dcf4649d6535
Oksana Mashyntseva <67964372+ksumm@users.noreply.github.com>
2022-10-10 15:24:14 +0100

    Added the relevant icon from API

--- src/app.js
+++ src/app.js
@@ -26,10 +26,14 @@ function displayTemperature(response){
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "c197bfddbccf715635ed21140b4bacbb";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Limerick&appid=${apiKey}&units=metric`;
+let city = "Limerick";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
+
