a08e1ee9726f2de716e7e5b70bad78f1f4f23b9b
Oksana Mashyntseva <67964372+ksumm@users.noreply.github.com>
2022-10-12 11:17:23 +0100

    Implemented units conversion

--- src/app.js
+++ src/app.js
@@ -46,10 +46,18 @@ function handleSubmit(event){
 function displayFahrenheitTemperature(event){
   event.preventDefault();
   let fahrenheitTemperature = (celsiusTemperature*9)/5+32;
-  alert(fahrenheitTemperature);
+  celsiusLink.classList.remove("active");
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 
+}
+
+function displayCelsiusTemperature(event){
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
 let celsiusTemperature = null;
@@ -58,6 +66,9 @@ let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature)
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature)
 
 search("Dublin");
